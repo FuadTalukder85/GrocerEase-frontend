51b00d224027394e844c13d0ce849bf9310723ef
FuadTalukder85 <webfuadtk@gmail.com>
2024-03-27 01:49:59 +0600

    verify image

--- src/app/(dashboardLayout)/dashboard/page.tsx
+++ src/app/(dashboardLayout)/dashboard/page.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
   return (
-    <div>
+    <div className="p-5">
       <h1>This is dashboard home</h1>
+      <Link href="/addProducts">
+        <button className="mt-5 bg-[#5DD2C0] px-5 py-2 rounded-md  text-white font-medium">
+          Add Products
+        </button>
+      </Link>
     </div>
   );
 };
