00c2eb611172c5fc89dcad3f6f24a3ed93fbbfff
FuadTalukder85 <webfuadtk@gmail.com>
2024-03-27 19:53:17 +0600

    get all products on dashboard table

--- src/app/(dashboardLayout)/dashboard/all-products/page.tsx
+++ src/app/(dashboardLayout)/dashboard/all-products/page.tsx
@@ -1,13 +1,28 @@
 import AllProductsTable from "@/components/allProductsTable/AllProductsTable";
 import React from "react";
 
-const AllProducts = () => {
+const AllProducts = async () => {
+  const res = await fetch(`${process.env.BACKEND_URL}/all-product`, {
+    next: {
+      revalidate: 30,
+    },
+  });
+  const allProductsTable = await res.json();
+  console.log(allProductsTable);
+  //
+  const allStates = await fetch(`${process.env.BACKEND_URL}/all-stats`, {
+    next: {
+      revalidate: 30,
+    },
+  });
+  const allState = await allStates.json();
+  console.log(allState);
   return (
     <div className="p-5">
-      <h1 className="text-3xl font-semibold text-[#333333]">
-        All Products : 703
-      </h1>
-      <AllProductsTable></AllProductsTable>
+      <AllProductsTable
+        allProductsTable={allProductsTable}
+        allState={allState}
+      ></AllProductsTable>
     </div>
   );
 };
--- src/components/allProductsTable/AllProductsTable.tsx
+++ src/components/allProductsTable/AllProductsTable.tsx
@@ -1,54 +1,78 @@
+"use client";
 import Image from "next/image";
-import React from "react";
-import img from "../../assets/product/product1.jpg";
+import { MdEditSquare } from "react-icons/md";
+import { AiFillDelete } from "react-icons/ai";
+import { AllProductValues } from "../types.tsx/types";
 
-const AllProductsTable = () => {
+const AllProductsTable = ({
+  allProductsTable,
+  allState,
+}: {
+  allProductsTable: AllProductValues[];
+}) => {
+  console.log("allProductsTable >", allProductsTable);
   return (
     <div className="mt-5">
-      <div className="overflow-x-auto">
+      <h1 className="text-3xl font-semibold text-[#333333]">
+        All Products :{" "}
+        <span className="text-[#E85363]">{allState.totalProducts}</span>
+      </h1>
+      <div className="overflow-x-auto mt-5">
         <table className="table">
           {/* head */}
           <thead>
             <tr>
               <th className="text-lg text-[#333333]">SL NO.</th>
-              <th className="text-lg text-[#333333]">Product Items</th>
+              <th className="text-lg text-[#333333]">Product Image</th>
+              <th className="text-lg text-[#333333]">Product Title</th>
               <th className="text-lg text-[#333333]">Product ID</th>
               <th className="text-lg text-[#333333]">Price</th>
-              <th className="text-lg text-[#333333]">Details</th>
-              <th className="text-lg text-[#333333]">Update</th>
-              <th className="text-lg text-[#333333]">Delete</th>
+              <th className="text-lg text-[#333333] text-center">Update</th>
+              <th className="text-lg text-[#333333] text-center">Delete</th>
             </tr>
           </thead>
           <tbody>
             {/* row 1 */}
-            <tr>
-              <th>
-                <label>1</label>
-              </th>
-              <td>
-                <div className="flex items-center gap-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      <Image src={img} alt="" />
+            {allProductsTable.map((product, index) => (
+              <tr key={product._id}>
+                <th>
+                  <label className="text-[#E85363] font-bold">
+                    {index + 1}
+                  </label>
+                </th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle">
+                        <Image
+                          src={product.image}
+                          alt="product"
+                          width={40}
+                          height={40}
+                        />
+                      </div>
                     </div>
                   </div>
+                </td>
+                <td>
                   <div>
-                    <div className="font-bold">Hart Hagerty</div>
+                    <div className="font-bold">{product.title}</div>
                   </div>
-                </div>
-              </td>
-              <td>
-                Zemlak, Daniel and Leannon
-                <br />
-                <span className="badge badge-ghost badge-sm">
-                  Desktop Support Technician
-                </span>
-              </td>
-              <td>Purple</td>
-              <th>
-                <button className="btn btn-ghost btn-xs">details</button>
-              </th>
-            </tr>
+                </td>
+                <td className="font-bold">{product._id}</td>
+                <td className="font-bold">${product.price}</td>
+                <td className="flex items-center justify-center text-[#E85363] text-4xl">
+                  <div className="flex items-center justify-center text-[#E85363] text-4xl">
+                    <MdEditSquare />
+                  </div>
+                </td>
+                <td>
+                  <div className="flex items-center justify-center text-[#E85363] text-4xl">
+                    <AiFillDelete />
+                  </div>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
